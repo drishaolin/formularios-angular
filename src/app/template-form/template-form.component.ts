a93e6f2e42d3d35624edcd91237ced9034341333
Adriana Mucciolo <drishaolin@gmail.com>
2022-06-18 19:03:28 -0300

    Validando campos do formulário

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: '',
+    email: ''
+  }
+  usuarioAuxiliarMsg: any = {
+    nome: 'Nome é obrigatório!',
+    email: 'Email inválido!'
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    //console.log(this.usuario);
+    
+  }
 
   constructor() { }
 
